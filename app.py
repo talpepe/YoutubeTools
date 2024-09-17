4891450ba643cbda66d1c28b669d42d3424b2ecf
Tal P <talpeperovits@gmail.com>
2024-09-17 17:48:34 +0300

    Adds logging

--- app.py
+++ app.py
@@ -182,4 +182,4 @@ def get_occurrences():
 if __name__ == '__main__':
     app.logger.debug('Starting Flask application...')
     app.logger.info('Starting Flask application...')
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',  port=80)
